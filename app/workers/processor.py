bc7bcfcd75b460f10c6694ff718fa49c18d8bc44
cqkp62mmd8-coder <cqkp62mmd8@privaterelay.appleid.com>
2026-05-16 02:44:58 +0300

    Create processor.py

---
+++ app/workers/processor.py
@@ -0,0 +1,17 @@
+import asyncio
+from core.queue import queue_manager
+from services.publisher import publish_message
+from core.logger import log
+
+async def worker_loop(worker_id: int):
+    log.info(f"Worker-{worker_id} started")
+
+    while True:
+        item = await queue_manager.pop()
+
+        try:
+            await publish_message(item)
+        except Exception as e:
+            log.error(f"Worker-{worker_id} error: {e}")
+
+        await asyncio.sleep(0.2)

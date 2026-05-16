3800ac5f010e48d382f874e204750c911230c902
cqkp62mmd8-coder <cqkp62mmd8@privaterelay.appleid.com>
2026-05-16 03:20:22 +0300

    Update logger.py

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,13 +1,18 @@
 from datetime import datetime
 
 class Logger:
+
     def info(self, msg):
+
         print(f"[{datetime.now().strftime('%H:%M:%S')}] [INFO] {msg}")
 
     def warn(self, msg):
+
         print(f"[{datetime.now().strftime('%H:%M:%S')}] [WARN] {msg}")
 
     def error(self, msg):
+
         print(f"[{datetime.now().strftime('%H:%M:%S')}] [ERROR] {msg}")
 
 log = Logger()
+

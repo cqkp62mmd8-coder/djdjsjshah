916fd054765532d82759465b27652db7937d9ac7
cqkp62mmd8-coder <cqkp62mmd8@privaterelay.appleid.com>
2026-05-16 03:38:59 +0300

    Update processor.py

--- app/workers/processor.py
+++ app/workers/processor.py
@@ -1,26 +1,43 @@
 import asyncio
+
 from core.queue import queue_manager
+
 from core.logger import log
 
 class ProcessorWorker:
 
     def __init__(self, publisher):
+
         self.publisher = publisher
 
-    async def run(self, worker_id: int):
+    async def run(self, worker_id):
 
-        log.info(f"Worker-{worker_id} started")
+        log.info(
+
+            f"Worker-{worker_id} online"
+
+        )
 
         while True:
+
             item = await queue_manager.pop()
 
             if not item:
+
                 continue
 
             try:
+
                 await self.publisher.publish(item)
 
             except Exception as e:
-                log.error(f"Worker-{worker_id} failed: {e}")
+
+                log.error(
+
+                    f"Worker-{worker_id} failed: {e}"
+
+                )
+
+                await queue_manager.fail(item)
 
             await asyncio.sleep(0.1)
